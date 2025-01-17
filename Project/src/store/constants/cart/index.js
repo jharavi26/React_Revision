bf347fe0cea28ac05ed7ac066107f4f7aad3ea52
Ravikumar Jha <jharavi153@gmail.com>
2025-01-17 12:54:50 +0530

    New Project

--- Project/src/store/constants/cart/index.js
+++
@@ -1,2 +0,0 @@
-export const REMOVE_CART = "REMOVE_CART";
-export const UPDATE_CART = "UPDATE_CART";
--- Project/src/store/constants/movie-list/index.js
+++
@@ -1,4 +0,0 @@
-export const UPDATE_ERROR = "UPDATE_ERROR";
-export const UPDATE_DATA = "UPDATE_DATA";
-export const UPDATE_CART = "UPDATE_CART";
-export const UPDATE_LOADER = "UPDATE_LOADER";

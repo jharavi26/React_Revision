47d3b1e9cd94fd1b326342b01843b3a0c1c4d6b0
Ravikumar Jha <jharavi153@gmail.com>
2025-02-05 11:51:29 +0530

    Basic Form Implementation

---
+++ Form/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Form1 from './Components/Form/Form1'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <Form1 />
+        
+       
+      </div>
+      
+    
+    </>
+  )
+}
+
+export default App

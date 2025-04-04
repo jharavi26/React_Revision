4b2463577f9ce9fa777da0b02a9d8f0f10ce2d84
Ravikumar Jha <jharavi153@gmail.com>
2025-04-04 13:55:55 +0530

    Add Code

--- context-api/src/App.jsx
+++ context-api/src/App.jsx
@@ -1,41 +1,25 @@
 import React, { useContext } from 'react';
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './components/Layout'
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Content from './Pages/Content'
 import { MyContext, MyContextProvider } from './context/Context';
+import Navbar from './components/Navbar';
 
-
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element : <Layout/>,
-    children : [
-      {
-        path : "/",
-        element : <Home/>
-      },
-      {
-        path : "/about",
-        element : <About/>
-      },
-      {
-        path : "/Content",
-        element : <Content/>,
-      }
-    ],
-  }
-])
 
 function App() {
   const { theme } = useContext(MyContext);
   return (
     <div className = {theme === "light" ? "light" : "dark"}>
-
-      <RouterProvider router={router}/>
-
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/content' element={<Content/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   )
 }
